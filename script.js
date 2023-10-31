5cc7a74a01b0b5e5952718391c7bada7c1aa4aec
maicon29 <maiconavelar30@gmail.com>
2023-10-31 14:45:03 -0300

    salv

---
+++ script.js
@@ -0,0 +1,13 @@
+let show = true;
+
+const conteudo = document.querySelector('.conteudo');
+
+const toggle = conteudo.querySelector('.toggle');
+
+toggle.addEventListener('click', () => {
+
+    document.body.style.overflow = show ? 'hidden' : 'initial'
+
+    conteudo.classList.toggle('on', show);
+    show = !show;
+})
